f9e6fc0f2537fcc96687b1ef988d7d1fc6157da2
GlebChickOFF <167457773+GlebChickOFF@users.noreply.github.com>
2024-04-29 22:53:20 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -2,7 +2,7 @@ my_list =['apple', 'lemon', 'strawberry', 'watermelon', 'peach']
 print(my_list)
 print(my_list[0])
 print(my_list[-1])
-print(my_list[2:])
+print(my_list[2:5])
 my_list[2]='orange'
 print(my_list)
 
